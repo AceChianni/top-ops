8e418c56a14a93c87c907d2c7c8cfef9656f0a0a
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2024-07-29 13:14:45 -0500

    Update Anime.js w/ props

--- src/app/components/Anime.js
+++ src/app/components/Anime.js
@@ -1,1 +1,13 @@
+import React from "react";
 
+export default function Anime(props) {
+  return (
+    <div className="p-5 m-5 border bg-orange-500 border-black rounded-md">
+      Anime: {props.anime}
+      <br></br>
+      Song: {props.song}
+      <br></br>
+      Artist: {props.artist}
+    </div>
+  );
+}
